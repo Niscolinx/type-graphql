5ff3c7d8420a8e5548d04aad68ff658cc687cd70
Igboanugwo Collins <munisco12@gmail.com>
2021-01-07 12:11:57 +0100

    typescript set up

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,33 @@
+import 'reflect-metadata'
 import { ApolloServer } from 'apollo-server-express'
 import * as Express from 'express'
-import { buildSchema } from 'type-graphql'
+import { buildSchema, Resolver, Query } from 'type-graphql'
 
 @Resolver()
-class RecipeResolver {
-    private recipesCollection: Recipe[] = [];
+class HelloResolver {
+    @Query(() => String)
 
-    async recipes() {
+    async hello() {
         // fake async in this example
-        return await this.recipesCollection;
+        return 'Hello World!'
     }
 }
 
 const main = async() => {
     const schema = await buildSchema({
-        resolvers: [FirstResolver, SecondResolver],
+        resolvers: [HelloResolver]
     })
 
 
-    const apolloServer = new ApolloServer({})
+    const apolloServer = new ApolloServer({schema})
+
+    const app = Express.default()
+    
+    apolloServer.applyMiddleware({app})
+
+    app.listen(4000, () => {
+        console.log('the server is running on port 4000')
+    })
 }
 
 main()
