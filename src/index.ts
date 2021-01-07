7ee3c38ff1ac879adffcb7e638053ddf1c39eca0
Igboanugwo Collins <munisco12@gmail.com>
2021-01-07 11:48:19 +0100

    typescript set up

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,23 @@ import { ApolloServer } from 'apollo-server-express'
 import * as Express from 'express'
 import { buildSchema } from 'type-graphql'
 
+@Resolver()
+class RecipeResolver {
+    private recipesCollection: Recipe[] = [];
+
+    async recipes() {
+        // fake async in this example
+        return await this.recipesCollection;
+    }
+}
+
 const main = async() => {
     const schema = await buildSchema({
         resolvers: [FirstResolver, SecondResolver],
     })
+
+
+    const apolloServer = new ApolloServer({})
 }
+
+main()
