f98a911fda345afee6bb85ec52853262e5068f16
Igboanugwo Collins <munisco12@gmail.com>
2021-01-07 11:42:49 +0100

    typescript set up

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { ApolloServer } from 'apollo-server-express'
+import * as Express from 'express'
+import { buildSchema } from 'type-graphql'
+
+const main = async() => {
+    const schema = await buildSchema({
+        resolvers: [FirstResolver, SecondResolver],
+    })
+}
