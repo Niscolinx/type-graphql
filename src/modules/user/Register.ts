df20991588803a30854efb4e2a74141ae3567164
Igboanugwo Collins <munisco12@gmail.com>
2021-01-08 11:56:18 +0100

    Arranged my file structure

---
+++ src/modules/user/Register.ts
@@ -0,0 +1,10 @@
+import { Resolver, Query } from 'type-graphql'
+
+@Resolver()
+export class RegisterResolver {
+    @Query(() => String)
+    async hello() {
+        // fake async in this example
+        return 'Hello World!'
+    }
+}
