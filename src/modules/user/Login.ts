46cacc7c9fed1964743b6f14edeb225e4055aea8
Igboanugwo Collins <munisco12@gmail.com>
2021-01-09 13:13:52 +0100

    working on the login resolver

--- src/modules/user/Login.ts
+++ src/modules/user/Login.ts
@@ -20,7 +20,7 @@ export class LoginResolver {
         const user = await User.findOne({ where: { email } })
 
         if (!user) {
-            return null
+            return firstName
         }
 
         const valid = await bcrypt.compare(password, user.password)
