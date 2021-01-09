25b035588683ea9edf4a61f702a79817eb364085
Igboanugwo Collins <munisco12@gmail.com>
2021-01-09 12:43:14 +0100

    Added the middleware session

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,40 @@
 import 'reflect-metadata'
 import { ApolloServer } from 'apollo-server-express'
-import * as Express from 'express'
-import { buildSchema} from 'type-graphql'
+import Express from 'express'
+import { buildSchema } from 'type-graphql'
 import { createConnection } from 'typeorm'
 import { RegisterResolver } from './modules/user/Register'
+import session from 'express-session'
 
-
-
-const main = async() => {
+const main = async () => {
     await createConnection()
 
     const schema = await buildSchema({
         resolvers: [RegisterResolver],
-        
     })
 
-    const apolloServer = new ApolloServer({schema})
+    const apolloServer = new ApolloServer({ schema })
 
-    const app = Express.default()
-    
-    apolloServer.applyMiddleware({app})
+    const app = Express()
+
+    app.use(
+        session({
+            store: new RedisStore({
+                client: redis as any,
+            }),
+            name: 'qid',
+            secret: 'sfsnfnsfs',
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                maxAge: 1000 * 60 * 60 * 24 * 7 * 365 //7 Years
+            }
+        })
+    )
+
+    apolloServer.applyMiddleware({ app })
 
     app.listen(4000, () => {
         console.log('Server is running on port http://localhost:4000/graphql')
