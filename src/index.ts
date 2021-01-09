90e7784c0b57101128ad357f07086ff486fcef21
Igboanugwo Collins <munisco12@gmail.com>
2021-01-09 12:50:29 +0100

    Added the middleware session

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ const main = async () => {
                 secure: process.env.NODE_ENV === 'production',
                 maxAge: 1000 * 60 * 60 * 24 * 7 * 365 //7 Years
             }
-        })
+        }) as any
     )
 
     apolloServer.applyMiddleware({ app })
