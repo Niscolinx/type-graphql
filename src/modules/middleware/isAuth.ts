89659323addd9e75baa4368e34a660c957a478da
Igboanugwo Collins <munisco12@gmail.com>
2021-02-20 17:30:18 +0100

    Working on middleware for guarding routes

--- src/modules/middleware/isAuth.ts
+++ src/modules/middleware/isAuth.ts
@@ -1,10 +1,13 @@
 import { MyContext } from './../../typing-stubs/MyContext';
 import { MiddlewareFn } from "type-graphql"
 
-export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
-   
-    console.log({context})
 
+declare module 'express-session' {
+    interface Session {
+        userId: number
+    }
+}
+export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
     if(!context.req.session.userId){
         throw new Error('Not authorized!')
     }
