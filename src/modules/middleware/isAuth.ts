af6c729d039746cd7f590efebb57c73d190ccbf9
Igboanugwo Collins <munisco12@gmail.com>
2021-02-20 17:28:08 +0100

    Working on middleware for guarding routes

--- src/modules/middleware/isAuth.ts
+++ src/modules/middleware/isAuth.ts
@@ -1,7 +1,13 @@
+import { MyContext } from './../../typing-stubs/MyContext';
 import { MiddlewareFn } from "type-graphql"
 
-export const isAuth: MiddlewareFn = async ({ context }, next) => {
+export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
    
     console.log({context})
+
+    if(!context.req.session.userId){
+        throw new Error('Not authorized!')
+    }
+
     return next()
 }
