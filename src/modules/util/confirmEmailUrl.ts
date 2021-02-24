6a8c3ab5f197746e1e06cea0253229db9c7f2149
Igboanugwo Collins <munisco12@gmail.com>
2021-02-24 20:25:26 +0100

    Added email confirmation

--- src/modules/util/confirmEmailUrl.ts
+++ src/modules/util/confirmEmailUrl.ts
@@ -2,7 +2,7 @@ import { redis } from '../../redis';
 import {v4} from 'uuid'
 
 
-export const confirmEmail = async(userId: number) => {
+export const confirmEmailUrl = async(userId: number) => {
     const token = v4()
 
     await redis.set(token, userId, 'ex', 60 * 60 * 24)
