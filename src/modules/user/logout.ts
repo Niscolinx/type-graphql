b5da2d562c236394e9f2ef346f2c5d1fa24e2205
Igboanugwo Collins <munisco12@gmail.com>
2021-02-25 20:45:16 +0100

    Cleared the cookie after logout

--- src/modules/user/logout.ts
+++ src/modules/user/logout.ts
@@ -13,6 +13,18 @@ export class LogoutResolver {
     async logout(@Ctx() ctx: MyContext): Promise<Boolean> {
         return new Promise((resolve, reject) => {
             
+            ctx.req.session.destroy((err) => {
+
+                if(err){
+                    console.log('error in destroying session', err)
+                    reject(false)
+                }
+
+                ctx.res.clearCookie('qid')
+                resolve(true)
+            })
+
+
         })
      
     }
