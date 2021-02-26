2fc93754277cb4da8374d1d01603ebfac76dad69
Igboanugwo Collins <munisco12@gmail.com>
2021-02-26 16:38:33 +0100

    Created a password mixin

--- src/modules/shared/sharedInput.ts
+++ src/modules/shared/sharedInput.ts
@@ -3,14 +3,13 @@ import { InputType, Field, ClassType } from 'type-graphql'
 
 export const PasswordMixin = <T extends ClassType>(BaseClass: T) => {
     @InputType()
-    class PasswordInput extends BaseClass {
+    class PasswordInp extends BaseClass {
         @Field()
         @Min(3)
         password: string
 
-        @Field()
-        ok: boolean
+     
     }
 
-    return PasswordInput
+    return PasswordInp
 }
--- src/modules/user/changePasswordInputs.ts
+++ src/modules/user/changePasswordInputs.ts
@@ -1,7 +1,8 @@
+import { PasswordMixin } from './../shared/sharedInput';
 import { InputType, Field } from 'type-graphql'
 
 @InputType()
-export class changePasswordInput{
+export class changePasswordInput extends PasswordMixin(class {}){
     @Field()
     token: string
 }
