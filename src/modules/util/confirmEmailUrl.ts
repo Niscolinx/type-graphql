2f0252073c3a7ad67b4d24fad8dabcb3becf75ed
Igboanugwo Collins <munisco12@gmail.com>
2021-02-27 21:55:10 +0100

    update

--- src/modules/util/confirmEmailUrl.ts
+++ src/modules/util/confirmEmailUrl.ts
@@ -6,6 +6,7 @@ import {v4} from 'uuid'
 export const confirmEmailUrl = async(userId: number) => {
     const token = v4()
 
+    console.log({token})
     await redis.set(confirmationToken + token, userId, 'ex', 60 * 60 * 24)
 
     return `http://localhost:3000/confirmEmail/${token}`
