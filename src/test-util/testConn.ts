9c482e11269e24910af4e0cb9b05199abb570c68
Igboanugwo Collins <munisco12@gmail.com>
2021-03-02 11:18:08 +0100

    Added test config

---
+++ src/test-util/testConn.ts
@@ -0,0 +1,14 @@
+export const testConn = (drop: boolean = false) => {
+    return {
+        name: 'default',
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'test',
+        database: 'test-example',
+        synchronize: drop,
+        logging: drop,
+        entities: [__dirname + '/../entity/*.*'],
+    }
+}
