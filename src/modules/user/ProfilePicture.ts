ffe77405ee94310e1e94101b7a37978091c18933
Igboanugwo Collins <munisco12@gmail.com>
2021-03-05 07:14:46 +0100

    Working on file upload

--- src/modules/user/ProfilePicture.ts
+++ src/modules/user/ProfilePicture.ts
@@ -1,10 +1,12 @@
 import { Resolver, Mutation, Arg } from "type-graphql";
 import { GraphQLUpload } from "graphql-upload";
+import { Upload } from "src/typing-stubs/Upload";
 
 @Resolver()
 export class ProfilePictureResolver{
     @Mutation(() => Boolean)
     async addProfilePicture(@Arg("picture", () => GraphQLUpload) {
-
+        createStream,
+        file
     }:Upload): Promise<Boolean>
 }
