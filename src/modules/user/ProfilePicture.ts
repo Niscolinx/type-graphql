92c5d0d8f859dc2688b0d0cae0dfbb18c71192f1
Igboanugwo Collins <munisco12@gmail.com>
2021-03-05 07:13:25 +0100

    Working on file upload

--- src/modules/user/ProfilePicture.ts
+++ src/modules/user/ProfilePicture.ts
@@ -1,1 +1,10 @@
-export const Profile
+import { Resolver, Mutation, Arg } from "type-graphql";
+import { GraphQLUpload } from "graphql-upload";
+
+@Resolver()
+export class ProfilePictureResolver{
+    @Mutation(() => Boolean)
+    async addProfilePicture(@Arg("picture", () => GraphQLUpload) {
+
+    }:Upload): Promise<Boolean>
+}
