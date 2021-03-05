16985eb5f3d2e1df0151ea08b0253cc0531327ea
Igboanugwo Collins <munisco12@gmail.com>
2021-03-05 07:20:01 +0100

    Working on file upload

--- src/modules/user/ProfilePicture.ts
+++ src/modules/user/ProfilePicture.ts
@@ -1,16 +1,20 @@
 import { Resolver, Mutation, Arg } from "type-graphql";
 import { GraphQLUpload } from "graphql-upload";
 import { Upload } from "src/typing-stubs/Upload";
+import { createWriteStream } from "fs";
 
 @Resolver()
 export class ProfilePictureResolver{
     @Mutation(() => Boolean)
     async addProfilePicture(@Arg("picture", () => GraphQLUpload) {
         createReadStream,
-        file
+        filename
     }:Upload): Promise<boolean> {
         return new Promise(async(resolve, reject) => {
-            createReadStream
+            createReadStream()
+            .pipe(createWriteStream(__dirname + `../../images/${filename}`))
+            .on('finish', () => resolve(true))
+            .on('error', () => reject(false))
         })
     }
 }
